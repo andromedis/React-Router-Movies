0f1800ced15875a58e3ab54fae3a914b041c5977
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-02-22 21:21:19 +0000

    got card linking in MovieList working

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+
 
 export default function MovieList(props) {
+  
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
@@ -11,10 +15,21 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+
+  const history = useHistory();
+
+  const routeToMovie = (id) => {
+    console.log(history);
+    console.log(id);
+    history.push(`/movies/${id}`);
+    console.log(history);
+    console.log(id);
+  }
 
   return (
-    <div className="movie-card">
+
+    <div className="movie-card" onClick={() => routeToMovie(id)}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -23,5 +38,6 @@ function MovieDetails(props) {
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
+
   );
 }
